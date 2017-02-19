2bfd77de8e9d48a8cf58115efc7f27cf4c3b3900
dzy <964660612@qq.com>
2017-02-19 22:32:31 +0800

    修改了登录注册时候的一些错误

--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,7 @@ router.post('/signin', function(req, res, next) {
     }
     if(user) {
       res.send({success: false, message: '用户名已存在', data:null});
+      return;
     }
     newUser.save(function(err, db) {
       if(err) {
@@ -30,15 +31,23 @@ router.post('/signin', function(req, res, next) {
 })
 
 router.post('/login', function(req, res, next) {
+  if(!req.body.name || !req.body.password) {
+    console.log('用户名或密码不能为空')
+    res.send({success:false, message:'用户名或密码不能为空', data:null});
+    return;
+  }
+  console.log('继续执行')
   User.get(req.body.name, function(err, user) {
     if(err) {
       res.send({success: false, message:'服务器出错', data:null});
+      return;
     }
     if(req.body.password == user.password) {
       req.session.user = user;
       res.send({success: true, message: '登录成功', data: user});
     }else{
       res.send({success: false, message:'用户名或密码错误', data:null});
+      return;
     }
   })
 })
